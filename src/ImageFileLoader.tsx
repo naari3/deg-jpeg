5348be2f43c876f2b784a6e8c86f3605f027e9fe
naari3 <naari.named@gmail.com>
2020-01-23 17:25:26 +0900

    Delete image view

--- src/ImageFileLoader.tsx
+++ src/ImageFileLoader.tsx
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
 
 const ImageFileLoader: React.FC = () => {
-  const [imgSrc, setImgSrc] = useState("");
+  const [, setImgSrc] = useState("");
 
   return (
     <div className="ImageFileLoader">
-      <img src={imgSrc} alt="" />
       <input
         type="file"
         onChange={e => {
           if (e.target.files === null) return;
 
           const file = e.target.files[0];
-          setImgSrc(URL.createObjectURL(file));
+          const src = URL.createObjectURL(file);
+          setImgSrc(src);
         }}
       />
     </div>
