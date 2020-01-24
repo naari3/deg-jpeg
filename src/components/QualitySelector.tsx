7faa871f9f77e0786eee14b4bd9d1e0f01f7e69b
naari3 <naari.named@gmail.com>
2020-01-24 18:33:54 +0900

    Wait a sec for user input completion

--- src/components/QualitySelector.tsx
+++ src/components/QualitySelector.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { ChangeQuality, GetStatus } from "../store/status";
 
 const QualitySelector: React.FC = () => {
   const dispatch = useDispatch();
-
   const status = useSelector(GetStatus);
+
+  const [currentQuality, setCurrentQuality] = useState(status.quality);
+  const [previousQuality, setPreviousQuality] = useState(status.quality);
+
+  useEffect(() => {
+    // wait for user input completion
+    setTimeout(() => {
+      if (currentQuality === previousQuality) {
+        dispatch(ChangeQuality({ quality: previousQuality }));
+      }
+      setPreviousQuality(currentQuality);
+    }, 200);
+  }, [currentQuality, previousQuality, dispatch]);
 
   return (
     <input
@@ -15,8 +27,8 @@ const QualitySelector: React.FC = () => {
       max={1.0}
       step={0.01}
       onChange={e => {
-        const quality = parseFloat(e.target.value);
-        dispatch(ChangeQuality({ quality }));
+        const currentQuality = parseFloat(e.target.value);
+        setCurrentQuality(currentQuality);
       }}
     />
   );
