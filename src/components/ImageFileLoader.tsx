1b34c56fc3e3b0d1756909e2d580bc6013b46e19
naari3 <naari.named@gmail.com>
2020-01-24 18:52:11 +0900

    Validate file

--- src/components/ImageFileLoader.tsx
+++ src/components/ImageFileLoader.tsx
@@ -13,6 +13,12 @@ const ImageFileLoader: React.FC = () => {
           if (e.target.files === null) return;
 
           const file = e.target.files[0];
+          if (!file.type.includes("image/")) {
+            alert("画像しか選択できません");
+            e.target.value = "";
+            return;
+          }
+
           const url = URL.createObjectURL(file);
 
           dispatch(ChangeImageUrl({ url }));
