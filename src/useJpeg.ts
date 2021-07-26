4b7728815fcbb0c059b607ea4e59abe2994291bd
naari3 <naari.named@gmail.com>
2021-07-26 16:51:06 +0900

    wasm-imagemagick test

--- src/useJpeg.ts
+++ src/useJpeg.ts
@@ -1,24 +1,31 @@
 import { useState, useCallback } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Status, GetStatus, ChangeJpegSet } from "./store/status";
+import { buildInputFile, call } from "wasm-imagemagick";
 
-const loadImage = (src: string) => {
-  return new Promise<HTMLImageElement>((resolve, reject) => {
-    const img = new Image();
-    img.onload = () => resolve(img);
-    img.onerror = e => reject(e);
-    img.src = src;
-  });
-};
+// const loadImage = (src: string) => {
+//   return new Promise<HTMLImageElement>((resolve, reject) => {
+//     const img = new Image();
+//     img.onload = () => resolve(img);
+//     img.onerror = e => reject(e);
+//     img.src = src;
+//   });
+// };
 
 const createJpeg = async (src: string, quality: number) => {
-  const canvas = document.createElement("canvas");
-  const image = await loadImage(src);
-  canvas.width = image.naturalWidth;
-  canvas.height = image.naturalHeight;
-  const ctx = canvas.getContext("2d");
-  ctx?.drawImage(image, 0, 0);
-  return canvas.toDataURL("image/jpeg", quality);
+  const imgSrc = await buildInputFile(src, 'image1.png');
+  const command = ["convert", imgSrc.name, '-quality', `${Math.floor((quality || 0.01) * 100)}`, 'out.jpeg'];
+  const result = await call([imgSrc], command);
+  const file = result.outputFiles[0];
+  return URL.createObjectURL(file.blob);
+
+  // const canvas = document.createElement("canvas");
+  // const image = await loadImage(src);
+  // canvas.width = image.naturalWidth;
+  // canvas.height = image.naturalHeight;
+  // const ctx = canvas.getContext("2d");
+  // ctx?.drawImage(image, 0, 0);
+  // return canvas.toDataURL("image/jpeg", quality);
 };
 
 export const useJpeg = (): [Status, () => Promise<void>, boolean, string] => {
@@ -31,10 +38,10 @@ export const useJpeg = (): [Status, () => Promise<void>, boolean, string] => {
   const getJpeg = useCallback(async () => {
     setLoading(true);
     try {
-      let dups = 5;
+      let dups = 1;
       const d = new URL(window.location.href).searchParams.get("d");
       if (d) {
-        dups = parseInt(d) || 5;
+        dups = parseInt(d) || 1;
       }
       const srces = await Array.from({ length: dups })
         .map(() => "")
